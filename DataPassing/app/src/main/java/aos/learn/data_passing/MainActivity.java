c205a79e916055fbb6a368fd096f8e35342cef64
chiaz91 <chiayu.dev@gmail.com>
2021-05-08 13:06:47 +0800

    init project

---
+++ DataPassing/app/src/main/java/aos/learn/data_passing/MainActivity.java
@@ -0,0 +1,14 @@
+package aos.learn.data_passing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
